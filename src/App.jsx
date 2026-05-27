812ce9e1462e185cb8f3bac55bab71f5d00ee462
shreyassindhur <shr.sin2.rt22@dypatil.edu>
2026-05-27 15:48:34 +0530

    fix: force rename navbar to Navbar on Windows

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
 import Experience from './components/Experience'
